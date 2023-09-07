13045420f4a203aa227db376ff96fb71626f13a0
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-07 12:23:05 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -12,7 +12,7 @@ class PolypDataset(Dataset):
             csv_file (str): Path to the CSV file with image file names and labels.
             transform (callable, optional): Optional transform to be applied to the image.
         """
-        self.data_dir = data_dir
+        self.data_dir = os.path.join(data_dir, 'images')  # Point to the 'images' subdirectory
         self.data_df = pd.read_csv(csv_file)
         self.transform = transform
 
@@ -20,38 +20,27 @@ class PolypDataset(Dataset):
         return len(self.data_df)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.data_dir, 'images', str(idx) + '.tif')
+        img_name = os.path.join(self.data_dir, str(self.data_df.iloc[idx, 0]) + '.tif')
         image = Image.open(img_name)
-
-        label = int(self.data_df.iloc[idx, 1])  # Assuming label is in the second column of the CSV file
+        label = int(self.data_df.iloc[idx, 1])
 
         if self.transform:
             image = self.transform(image)
 
         return image, label
 
-def create_data_loaders(data_dir, train_csv, test_csv, valid_csv, batch_size):
+def create_data_loaders(train_dir, test_dir, valid_dir, batch_size):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    train_dataset = PolypDataset(data_dir=data_dir,
-                                 csv_file=train_csv,
-                                 transform=transform)
-
-    test_dataset = PolypDataset(data_dir=data_dir,
-                                csv_file=test_csv,
-                                transform=transform)
-
-    valid_dataset = PolypDataset(data_dir=data_dir,
-                                 csv_file=valid_csv,
-                                 transform=transform)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(PolypDataset(data_dir=train_dir, csv_file=os.path.join(train_dir, 'train.csv'), transform=transform), batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(PolypDataset(data_dir=test_dir, csv_file=os.path.join(test_dir, 'test.csv'), transform=transform), batch_size=batch_size)
+    valid_loader = DataLoader(PolypDataset(data_dir=valid_dir, csv_file=os.path.join(valid_dir, 'valid.csv'), transform=transform), batch_size=batch_size, shuffle=True)
 
     return train_loader, test_loader, valid_loader
+
+
 
