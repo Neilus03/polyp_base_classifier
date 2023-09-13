c7e2b9138242fb4799b651b33dd87cb9b416ef5e
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-13 11:13:00 +0200

    now is effV2

--- model.py
+++ model.py
@@ -1,17 +1,16 @@
 import torch
 import torch.nn as nn
-from efficientnet_pytorch import EfficientNet
 
 class CustomEfficientNet(nn.Module):
     def __init__(self, num_classes):
         super(CustomEfficientNet, self).__init__()
 
         # Load pre-trained EfficientNetV2 model
-        self.backbone = EfficientNet.from_pretrained('efficientnetv2-b0')
-
-        # Modify the classifier head for binary classification
+        self.backbone = torch.hub.load('hankyul2/EfficientNetV2-pytorch', 'efficientnet_v2_s', pretrained=True, dropout=0.0, stochastic_depth=0.0)
+        
+        # Modify the classifier to your desired number of classes
         self.classifier = nn.Sequential(
-            nn.Linear(self.backbone._fc.in_features, 256),
+            nn.Linear(1000, 256),
             nn.ReLU(),
             nn.Linear(256, num_classes),
             nn.Sigmoid()  # Sigmoid for binary classification
@@ -25,4 +24,5 @@ class CustomEfficientNet(nn.Module):
 def initialize_model(num_classes):
     model = CustomEfficientNet(num_classes)
     return model
+
 
